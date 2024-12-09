b27ac852758fb671affba4cf92f9829d0542858f
naitikbarot1609 <naitikbarot99@gmail.com>
2024-12-09 18:52:26 -0500

    Initial Commit

---
+++ test/cicd2_new.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cicd2New from '../lib/cicd2_new-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cicd2_new-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cicd2New.Cicd2NewStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
