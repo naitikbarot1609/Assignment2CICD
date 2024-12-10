7fa0bbb303333d87ef6789a7d55405817b0ab2c8
naitikbarot1609 <naitikbarot99@gmail.com>
2024-12-09 19:12:05 -0500

    lib file modified

--- lib/cicd2_new-stack.ts
+++ lib/cicd2_new-stack.ts
@@ -2,25 +2,19 @@ import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-// import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
- 
-export class MyCdkProjectStack extends cdk.Stack {
+
+export class Cicd2NewStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
- 
-    // The code that defines your stack goes here
- 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyCdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    // Create an S3 bucket
     const myBucket = new s3.Bucket(this, 'MyBucket', {
       versioned: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY, // Automatically delete bucket for dev/test environments
     });
-   
+
+    // Create a Lambda function
     const myLambda = new lambda.Function(this, 'MyLambda', {
       runtime: lambda.Runtime.NODEJS_18_X, // Updated runtime
       handler: 'index.handler',
@@ -34,17 +28,15 @@ export class MyCdkProjectStack extends cdk.Stack {
         BUCKET_NAME: myBucket.bucketName,
       },
     });
-   
- 
+
+    // Create a DynamoDB table
     const myTable = new dynamodb.Table(this, 'MyTable', {
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
       tableName: 'MyTable',
       removalPolicy: cdk.RemovalPolicy.DESTROY, // Automatically delete for dev/test environments
     });
-   
-   
+
     // Grant the Lambda function read/write permissions to the S3 bucket
     myBucket.grantReadWrite(myLambda);
-   
   }
 }
